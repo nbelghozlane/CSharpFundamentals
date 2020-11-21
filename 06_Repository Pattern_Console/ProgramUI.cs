846cb38440b004e2d9fd122f26a2572196d5ea35
Nadia <nbelghoz@gmail.com>
2020-11-21 11:54:46 -0500

    Added code for Video 4 ProgramUI Create Method

--- 06_Repository Pattern_Console/ProgramUI.cs
+++ 06_Repository Pattern_Console/ProgramUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _06_RepositoryPattern_Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,9 @@ namespace _06_Repository_Pattern_Console
 {
     class ProgramUI
     {
+        private StreamingContentRepository _contentRepo = new StreamingContentRepository();
+
+
         //Method that runs/starts the application (UI part)
         public void Run()
         {
@@ -72,6 +76,56 @@ namespace _06_Repository_Pattern_Console
         //Create new Streaming Content
         private void CreateNewContent()
         {
+            Console.Clear();
+            StreamingContent newContent = new StreamingContent();
+
+            // Title
+            Console.WriteLine("Enter the title for the content:");
+            newContent.Title = Console.ReadLine();
+
+            //Description
+            Console.WriteLine("Enter the description for the content:");
+            newContent.Description = Console.ReadLine();
+
+            //Maturity Rating
+            Console.WriteLine("Enter the rating for the content (G, PG, PG-13, etc.):");
+            newContent.MaturityRating = Console.ReadLine();
+
+            //Star Rating
+            Console.WriteLine("Enter the star count for the content (5.8, 10, 1.5, etc.)");
+            string starsAsString = Console.ReadLine();
+            newContent.StarRating = double.Parse(starsAsString);
+
+            //IsFamilyFriendly
+            Console.WriteLine("Is this content family friendly? (y/n)");
+            string familyFriendlyString = Console.ReadLine().ToLower();  //.ToLower means that if user inputs an uppercase letter (Y/N), it wil automatically be converted to lowercase.
+
+            if(familyFriendlyString == "y")
+            {
+                newContent.IsFamilyFriendly = true;
+            }
+            else
+            {
+                newContent.IsFamilyFriendly = false;
+            }
+
+
+            //GenreType
+
+            Console.WriteLine("Enter the Genre Number:\n" +
+                "1. Horror\n" +
+                "2. RomCom\n" +
+                "3. SciFi\n" +
+                "4. Documentary\n" +
+                "5. Bromance\n" +
+                "6. Drama\n" +
+                "7. Action");
+
+            string genreAsString = Console.ReadLine();
+            int genreAsInt = int.Parse(genreAsString);   // .Parse took string and converted it into an integer (number)
+            newContent.TypeOfGenre = (GenreType)genreAsInt;
+
+            _contentRepo.AddContentToList(newContent);
 
         }
 
